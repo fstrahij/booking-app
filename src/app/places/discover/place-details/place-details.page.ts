81c42d0b741f55896eefdcba8fd05ee2bf3a53d1
Filip Strahija <filip2893@gmail.com>
2025-04-22 15:20:28 +0200

    pages added

---
+++ src/app/places/discover/place-details/place-details.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-details',
+  templateUrl: './place-details.page.html',
+  styleUrls: ['./place-details.page.scss'],
+  standalone: false
+})
+export class PlaceDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Place} from "./place.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places : Place[]= [
+      new Place('p1', 'Manhatan Mansion', 'In the heart of New York City', 'https://s1.1zoom.me/big3/366/421441-svetik.jpg', 149.99),
+      new Place('p2', 'Zagreb', 'Zagreb volim te', 'https://www.roadaffair.com/wp-content/uploads/2020/03/aerial-view-zagreb-croatia-shutterstock_1199253325.jpg', 89.99),
+      new Place('p3', 'Ljubljana', 'Ljubljana srce Slovenije', 'https://adventurousmiriam.com/wp-content/uploads/2015/06/Ljubljana.jpg', 99.99),
+  ];
+
+  public GetPlaces(){
+    return [...this._places];
+  }
+
+  constructor() { }
+}
