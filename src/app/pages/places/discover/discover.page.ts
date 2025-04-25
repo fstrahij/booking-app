86664702c3f587bd176daff4e7a787c5a64faa54
Filip Strahija <filip2893@gmail.com>
2025-04-25 12:50:42 +0200

    sement button added

--- src/app/pages/places/discover/discover.page.ts
+++ src/app/pages/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PlacesService} from "../../../services/places/places.service";
 import {Place} from "../../../models/place.model";
+import {SegmentChangeEventDetail} from "@ionic/angular";
 
 @Component({
   selector: 'app-discover',
@@ -17,4 +18,7 @@ export class DiscoverPage implements OnInit {
     this.loadedPlaces = this.placeService.places;
   }
 
+  onFilterChange(event: CustomEvent<SegmentChangeEventDetail>){
+    console.log(event.detail);
+  }
 }
