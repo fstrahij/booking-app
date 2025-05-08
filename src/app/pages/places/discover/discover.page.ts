6aefab036bbc45b76ed140ef1cf4b8c2454a79e1
Filip Strahija <filip2893@gmail.com>
2025-05-08 10:17:03 +0200

    discover places segments implemented

--- src/app/pages/places/discover/discover.page.ts
+++ src/app/pages/places/discover/discover.page.ts
@@ -4,6 +4,7 @@ import {Subject, takeUntil} from "rxjs";
 import {SegmentChangeEventDetail} from "@ionic/angular";
 
 import {PlacesService} from "../../../services/places/places.service";
+import {AuthService} from "../../../services/auth/auth.service";
 import {Place} from "../../../models/place.model";
 
 @Component({
@@ -14,18 +15,37 @@ import {Place} from "../../../models/place.model";
 })
 export class DiscoverPage implements OnInit, OnDestroy {
   destroyed$ = new Subject();
-  loadedPlaces: Place[] = [];
+  allPlaces: Place[] = [];
+  showedPlaces: Place[] = [];
+  featuredPlace: Place;
 
-  constructor(private placeService: PlacesService) { }
+  constructor(private placeService: PlacesService,
+              private authService: AuthService
+  ) { }
 
   ngOnInit() {
     this.placeService
         .places
         .pipe(takeUntil(this.destroyed$))
-        .subscribe(places => this.loadedPlaces = places);
+        .subscribe(places => {
+          if (places && places.length > 0) {
+            this.allPlaces = this.showedPlaces = places;
+            this.featuredPlace = places[0];
+          }
+        });
   }
 
   onFilterChange(event: CustomEvent<SegmentChangeEventDetail>){
+    if(event.detail.value === 'all'){
+      this.featuredPlace = {...this.allPlaces[0]};
+      this.showedPlaces = [...this.allPlaces];
+    }
+    else if(event.detail.value === 'bookable'){
+      this.showedPlaces = [...this.allPlaces.filter(place => place.userId !== this.authService.userId)];
+      if(this.showedPlaces.length > 0){
+        this.featuredPlace = {...this.showedPlaces[0]};
+      }
+    }
     console.log(event.detail);
   }
 
